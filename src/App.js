d335e1636d6cff114656b876341eae336e01b0e0
Ramit <ramit.seth.developer@gmail.com>
2021-02-21 20:09:02 +0530

    Added-note-styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import { Note } from './components'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      hello
+      <h1 className="main-heading"><span className="one">k</span><span className="two">e</span><span className="three">e</span><span className="four">p</span></h1>
+      <Note />
     </div>
   );
 }
---
+++ src/components/Note.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Note = () => {
+    return (
+        <div className="note">
+            <button className="add-note">Add Note</button>
+        </div>
+    )
+}
+
+
